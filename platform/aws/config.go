5f38618ed1c9a3b7c6f0748c288ee060490bf534
jake <lee@lou2.kr>
2023-03-08 14:07:49 +0900

    mod, 키값 조회

--- platform/aws/config.go
+++ platform/aws/config.go
@@ -2,14 +2,20 @@ package aws
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
+	"log"
 	"os"
 )
 
 func awsConfig() (aws.Config, error) {
 	clientKey := os.Getenv("AWS_CLIENT_KEY")
+	fmt.Println("클라이언트키")
+	log.Println("클라이언트키")
+	fmt.Println(clientKey)
+	log.Println(clientKey)
 	secretKey := os.Getenv("AWS_CLIENT_SECRET")
 	return config.LoadDefaultConfig(
 		context.TODO(),
